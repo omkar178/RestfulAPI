f355b43a8fd83c363cedc0b694445c6e853c7a16
omi Navik <navik46@gmail.com>
2022-02-11 18:02:35 +0530

    Added post methos in nationalpark controller to create new national park

--- RestfulAPI/Controllers/NationalParkController.cs
+++ RestfulAPI/Controllers/NationalParkController.cs
@@ -5,6 +5,7 @@ using RestfulAPI.Model.Dtos;
 using RestfulAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RestfulAPI.Controllers
 {
@@ -22,21 +23,60 @@ namespace RestfulAPI.Controllers
 
         [HttpGet]
         public IActionResult GetAllNationalPark()
-        {               
+        {
             var objList = _npRepo.GetAllNationalParks();
             var objDto = new List<NationalParkDto>();
 
-             if (objList != null)
-             {
+            if (objList != null && objList.Count() > 0 )
+            {
                 foreach (NationalPark np in objList)
                 {
-                   objDto.Add(_mapper.Map<NationalParkDto>(np));
+                    objDto.Add(_mapper.Map<NationalParkDto>(np));
                 }
                 return Ok(objDto);
-             }
-             return NotFound();
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{Id:int}")]
+        public IActionResult GetNationalPark(int Id)
+        {
+            var nationalPark = _npRepo.GetNationalPark(Id);
+            if (nationalPark == null)
+            {
+                return NotFound();
+            }
+
+            var objDto = _mapper.Map<NationalParkDto>(nationalPark);
+            return Ok(objDto);
             
-            
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            { 
+                return BadRequest(ModelState);
+            }
+            if (_npRepo.IsExist(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("",$"NationalPark {nationalParkDto.Name} already exist");
+                return StatusCode(404,ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Obj = _mapper.Map<NationalPark>(nationalParkDto);
+            if (!_npRepo.CreateNationalPark(Obj))
+            {
+                ModelState.AddModelError("", $"Somethind Wrong happned while adding {Obj.Name} to the database");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok();
         }
     }
 }
