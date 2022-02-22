7552e478e78e7cdd4ba8589157db22008c54af95
omi Navik <navik46@gmail.com>
2022-02-23 00:13:24 +0530

    Add GetAll trails and Get individual trail methos in to trailcontroller

--- RestfulAPI/Controllers/TrailController.cs
+++ RestfulAPI/Controllers/TrailController.cs
@@ -1,7 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestfulAPI.Model;
+using RestfulAPI.Model.Dtos;
 using RestfulAPI.Repository.IRepository;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RestfulAPI.Controllers
 {
@@ -26,6 +30,47 @@ namespace RestfulAPI.Controllers
         {
             _trailsRepository = trailsRepository;
             _mapper = mapper;
+        }
+        /// <summary>
+        /// Get All trails
+        /// </summary>
+        /// <returns></returns>       
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public IActionResult GetAllTrail()
+        { 
+            var objTrail = _trailsRepository.GetAllTrails();
+            var objTrailDto = new List<TrailDtos>();
+            if (objTrail != null && objTrail.Count() > 0)
+            {
+                foreach (Trail trail in objTrail)
+                {
+                    objTrailDto.Add(_mapper.Map<TrailDtos>(trail));
+                }
+                return Ok(objTrailDto);
+            }
+            return NotFound();
+        }
+        /// <summary>
+        /// Get a Individual trail.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpGet("{Id:int}",Name = "GetTrail")]
+        [ProducesDefaultResponseType]
+        public IActionResult GetTrail(int Id)
+        {
+            var objTrail = _trailsRepository.GetTrail(Id);
+            if (objTrail == null)
+            {
+                return NotFound();
+            }
+
+            var objTrailDto = _mapper.Map<TrailDtos>(objTrail);
+            return Ok(objTrailDto);
+            
         }
        
     }
